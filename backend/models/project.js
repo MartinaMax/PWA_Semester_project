168000f5e43bb506b0274ab5b834a862309c9d5b
MartinaMax <mmaximova37@gmail.com>
2024-04-18 21:01:57 +0200

    Project model and route created

---
+++ backend/models/project.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Project schema
+let projectSchema = new Schema (
+
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true,
+            max: 255
+        },
+        startDate: {
+            type: Date,
+            required: true,
+            default: Date.now
+        },
+        endDate: {
+            type: Date,
+            required: true,
+        },
+        author: {
+            type: Schema.Types.ObjectId, 
+            ref: 'user', 
+            required: true
+        },
+        task: {
+            type: Schema.Types.ObjectId, 
+            ref: 'task', 
+        }
+    }
+);
+
+module.exports = mongoose.model("project", projectSchema);
---
+++ backend/routes/project.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const project = require("../models/project");
+//const user = require("../models/trail");
+//const {tokenVerification} = require("../validation");
+
+
+// GET - Fetch all project based on the userID (tokenVerification)
+router.get("/", (req, res) => {
+
+    
+    project.find()
+    .then(data => {res.send(data);})
+    .catch(err => {res.status(500).send(JSON.stringify(err));})
+
+});
+
+
+// POST - Create a new project in the database (tokenVerification)
+router.post("/", (req, res) => {
+   
+    data = req.body;
+
+    project.insertMany(data)
+    .then(data => {res.send(data);})
+    .catch(err => {res.status(500).send(JSON.stringify(err));})
+  
+});
+
+
+// UPDATE - Update a project based on the ID (tokenVerification)
+router.put("/:id", (req, res) => {
+   
+    const id = req.params.id;
+
+
+    project.findByIdAndUpdate(id, req.body)
+    .then(data => {
+        if(!data){
+            res.status(404).send({message: "Wrong project id" + id + "."})
+        }
+        else{
+            res.send({message: "The project information was modified."})
+        }
+
+    })
+    .catch(err => {res.status(500).send(JSON.stringify(err)); })
+});
+
+
+// DELETE - Delete a project based on the ID (tokenVerification)
+router.delete("/:id", (req, res) => {
+   
+    const id = req.params.id;
+
+
+    project.findByIdAndDelete(id)
+    .then(data => {
+        if(!data){
+            res.status(404).send({message: "Cannot delete project with id" + id + "."})
+        }
+        else{
+            res.send({message: "The project was deleted."})
+        }
+
+    })
+    .catch(err => {res.status(500).send(JSON.stringify(err)); })
+});
+
+module.exports = router;
