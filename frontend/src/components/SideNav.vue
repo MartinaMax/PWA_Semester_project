e06b15c61f1a34bff2ce9c829847426b48d38fe8
MartinaMax <mmaximova37@gmail.com>
2024-05-03 18:27:38 +0200

    Side navigation component

--- frontend/src/components/SideNav.vue
+++ frontend/src/components/SideNav.vue
@@ -1,13 +1,13 @@
 <template>
-    <article class="sidenav">
-      <img src="" alt="Logo image">
-      <h3>Dashboard</h3>
-      <div>
-        <img src="" alt="Profile photo" class="margin-b-15">
-        <h3 class="margin-b-30">Account</h3>
-        <a href=""><p>Log out</p></a>
-      </div>
-    </article>
+  <article class="sidenav">
+    <img src="" alt="Logo image" class="margin-b-50">
+    <router-link to="/dashboard"><h3>Dashboard</h3></router-link>
+    <div class="account-section">
+      <img src="../assets/avatar.png" alt="Profile photo" class="avatar-img margin-b-15">
+      <h3 class="margin-b-30">Account</h3>
+      <button>Log out</button>
+    </div>
+  </article>
 </template>
   
 <script>
@@ -16,11 +16,33 @@
   }
 </script>
 
-<style scoped lang="scss">
-    .sidenav {
-        width: 250px;
-        padding: 50px 30px;
-        background-color: black;
-        color: azure;
-    }
+<style scoped>
+  .sidenav {
+    text-align: center;
+    width: 250px;
+    padding: 50px 30px;
+    background-color: black;
+    color: white;
+  }
+
+  .sidenav > a {
+    color: white;
+  }
+
+  .account-section {
+    position: absolute;
+    bottom: 30px;
+  }
+
+  .avatar-img {
+    width: 50px;
+  }
+
+  button{
+    font-size: 18px;
+    background-color: white;
+    padding: 15px 50px;
+    border: none;
+  }
+
 </style>
